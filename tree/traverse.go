40e5ab16c87f4d786e1254c9ffee5db5bed43dc6
ricolai <ricolai@guojiang.tv>
2024-03-07 11:50:59 +0800

    feat: rename file

--- tree/traverse.go
+++ tree/traverse.go
@@ -1,37 +1,5 @@
 package tree
 
-import (
-	"strconv"
-)
-
-type TreeNode struct {
-	Left  *TreeNode
-	Right *TreeNode
-	Val   int
-}
-
-func BuildTreeByArr(data []string, index int) *TreeNode {
-	if len(data) == 0 || index >= len(data) || data[index] == "#" {
-		return nil
-	}
-
-	n, err := strconv.ParseInt(data[index], 10, 64)
-	if err != nil {
-		panic(err)
-	}
-
-	root := &TreeNode{
-		Left:  nil,
-		Right: nil,
-		Val:   int(n),
-	}
-
-	root.Left = BuildTreeByArr(data, 2*index+1)
-	root.Right = BuildTreeByArr(data, 2*index+2)
-
-	return root
-}
-
 func PreOrderRecursive(root *TreeNode) (res []int) {
 	if root == nil {
 		return nil
