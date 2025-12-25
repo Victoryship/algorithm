8ad81f0dbd23ed9d4674be941ca0a6392050d884
rico <ricolai@guojiang.tv>
2025-12-25 09:45:41 +0800

    feat: 添加回溯算法

--- dfs/dfs_test.go
+++ dfs/dfs_test.go
@@ -5,6 +5,50 @@ import (
 	"testing"
 )
 
+func TestLetterCombinations(t *testing.T) {
+	testCases := []struct {
+		nums string
+		want []string
+	}{
+		{"23", []string{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"}},
+		{"2", []string{"a", "b", "c"}},
+		{"9", []string{"w", "x", "y", "z"}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%v,%v", tc.nums, tc.want), func(t *testing.T) {
+			got := LetterCombinations(tc.nums)
+			for i := 0; i < len(got); i++ {
+				if got[i] != tc.want[i] {
+					t.Errorf("LetterCombinations(%s) = %s, want %s", tc.nums, got[i], tc.want[i])
+				}
+			}
+		})
+	}
+}
+
+func TestLetterCombinations1(t *testing.T) {
+	testCases := []struct {
+		num int
+		want []string
+	}{
+		{3, []string{"000", "001", "010", "011", "100", "101", "110", "111"}},
+		{2, []string{"00", "01", "10", "11"}},
+		{1, []string{"0", "1"}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%d,%v", tc.num, tc.want), func(t *testing.T) {
+			got := LetterCombinations1(tc.num)
+			for i := 0; i < len(got); i++ {
+				if got[i] != tc.want[i] {
+					t.Errorf("LetterCombinations(%d) = %s, want %s", tc.num, got[i], tc.want[i])
+				}
+			}
+		})
+	}
+}
+
 func TestPermute(t *testing.T) {
 	testCases := []struct {
 		nums []int
